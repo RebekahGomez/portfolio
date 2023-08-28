95ab35638fa4c1f65cebb31653de15d2ef6d1ec7
Rebekah <rebekahmenn@gmail.com>
2023-08-27 20:00:43 -0400

    updated About Me with expanded text, media query

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AboutMe.css";
 
 const AboutMe = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const truncatedText =
+    "Hi, I'm Rebekah Gomez! I'm a passionate developer, avid reader, foodie...";
+
+  const fullText = `Hi, I'm Rebekah Gomez! I'm a passionate developer, avid reader, foodie, and
+  bull-terrier mom. With a strong background in JavaScript, React, and
+  other web technologies, I'm always eager to take on new challenges and
+  create impactful, user-friendly applications. Please take a look at some
+  of my favorite projects, and feel free to reach out via LinkedIn!`;
+
   return (
     <div className="about-me">
       <h2>About Me</h2>
       <p>
-        Hi, I'm Rebekah Gomez! I'm a passionate developer, an avid reader, and
-        bull-terrier mom. With a strong background in JavaScript, React, and
-        other web technologies, I'm always eager to take on new challenges and
-        create impactful, user-friendly applications. Please take a look at some
-        of my favorite projects, and feel free to reach out via LinkedIn!
+        {isExpanded ? fullText : truncatedText}
+        <span className="read-more" onClick={() => setIsExpanded(!isExpanded)}>
+          {isExpanded ? " Show Less" : " Read More"}
+        </span>
       </p>
     </div>
   );
