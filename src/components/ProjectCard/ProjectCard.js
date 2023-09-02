f834376bcf8208227e4979b78ab23b4e9a699a88
Rebekah <rebekahmenn@gmail.com>
2023-09-01 20:40:03 -0400

    modal work

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -4,9 +4,8 @@ import "./ProjectCard.css";
 const ProjectCard = ({ project, onDetailsClick }) => {
   return (
     <div className="project-card">
-      <img src={project.image} alt={project.title} className="project-image" />
       <h3>{project.title}</h3>
-      <p>{project.description}</p>
+      <img src={project.image} alt={project.title} className="project-image" />
       <button onClick={onDetailsClick}>Details</button>
     </div>
   );
--- src/components/ProjectModal/ProjectModal.js
+++ src/components/ProjectModal/ProjectModal.js
@@ -2,14 +2,35 @@ import React from "react";
 import "./ProjectModal.css";
 
 const ProjectModal = ({ project, onClose }) => {
+  const handleLinkClick = (url) => {
+    window.open(url, "_blank");
+  };
+
   return (
-    <div className="project-modal">
-      <button onClick={onClose}>Close</button>
-      <h2>{project.title}</h2>
-      <img src={project.image} alt={project.title} />
-      <p>{project.description}</p>
-      <a href={project.githubLink}>GitHub</a>
-      <a href={project.deployedLink}>Deployed Site</a>
+    <div className="project-modal" onClick={onClose}>
+      <div
+        className="project-modal-content"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button className="close-button" onClick={onClose}>
+          X
+        </button>
+        <h2>{project.title}</h2>
+        <img src={project.image} alt={project.title} />
+        <p>{project.description}</p>
+        <button
+          className="project-link"
+          onClick={() => handleLinkClick(project.githubLink)}
+        >
+          GitHub
+        </button>
+        <button
+          className="project-link"
+          onClick={() => handleLinkClick(project.deployedLink)}
+        >
+          Deployed Site
+        </button>
+      </div>
     </div>
   );
 };
