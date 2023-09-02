8f5ff799085e35f763605e505e1c7cd979a8f8ef
Rebekah <rebekahmenn@gmail.com>
2023-09-01 21:02:52 -0400

    stop background scroll

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ProjectCard from "../ProjectCard/ProjectCard";
 import ProjectModal from "../ProjectModal/ProjectModal";
 import projectsData from "../../assets/data/projects.json";
@@ -6,6 +6,14 @@ import "./Projects.css";
 
 const Projects = () => {
   const [selectedProject, setSelectedProject] = useState(null);
+
+  useEffect(() => {
+    if (selectedProject) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+  }, [selectedProject]);
 
   const handleDetailsClick = (project) => {
     setSelectedProject(project);
