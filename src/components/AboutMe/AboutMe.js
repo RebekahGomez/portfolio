beb83fcfbbb022919be4dfa83975971f9ac6206f
Rebekah <rebekahmenn@gmail.com>
2023-11-27 15:35:41 -0500

    changed bio

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -14,11 +14,9 @@ const AboutMe = () => {
   const truncatedText =
     "Hi, I'm Rebekah Gomez! I'm a passionate developer, avid reader, foodie...";
 
-  const fullText = `Hi, I'm Rebekah Gomez! I'm a passionate developer, avid reader, foodie, and
-  bull-terrier mom. With a strong background in JavaScript, React, and
-  other web technologies, I'm always eager to take on new challenges and
-  create impactful, user-friendly applications. Please take a look at some
-  of my favorite projects, and feel free to reach out via LinkedIn!`;
+  const fullText = `Welcome! I'm a passionate developer, avid reader, and New York transplant. With a background in JavaScript, React, and
+  other web technologies, I'm motivated to take on new challenges and
+  create impactful, user-friendly applications. Thank you for taking the time to look at some of my favorite projects, and feel free to reach out via LinkedIn.`;
 
   return (
     <div className="about-me">
