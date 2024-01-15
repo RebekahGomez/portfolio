698baf1cfb6caeb3f58634c0a17ad0573bb2f1ea
Rebekah <rebekahmenn@gmail.com>
2024-01-15 15:24:13 -0500

    updated About Me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -14,9 +14,10 @@ const AboutMe = () => {
   const truncatedText =
     "Welcome! I'm a passionate developer, avid reader, and New York...";
 
-  const fullText = `Welcome! I'm a passionate developer, avid reader, and New York transplant. With a background in JavaScript, React, and
-  other web technologies, I'm motivated to take on new challenges and
-  create impactful, user-friendly applications. Thank you for taking the time to look at some of my favorite projects, and feel free to reach out via LinkedIn.`;
+  const fullText = `Welcome! I'm a passionate developer and data analyst. With a background in JavaScript, React, Python and
+  other web technologies, I'm motivated to take on new challenges and create impactful, user-friendly applications.
+  Data Analysis excites me the most; I love finding outliers in a dataset and bringing those discoveries to life with visualizations using Tableau.
+  Thank you for taking the time to look at some of my favorite projects, and feel free to reach out via LinkedIn.`;
 
   return (
     <div className="about-me">
